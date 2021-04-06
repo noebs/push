4274cc709b1cc80859906aebf98496acb969f399
Mohamed Yousif <mmbusif@gmail.com>
2021-04-06 18:33:28 +0200

    small changes

--- main_test.go
+++ main_test.go
@@ -16,7 +16,7 @@ func TestNewMessage(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		{"testing successful message1223", args{title: "Hear hear", body: "yeah that would do it!", topic: "general"}, false},
+		{"testing successful messagee7", args{title: "Service Notice", body: "Dear Cashq users, we are running through EBS issues now. Will feedback promptly!", topic: "general"}, false},
 		{"testing successful message", args{title: "Hear hearj", body: "yeah that would do it!", topic: "general"}, false},
 	}
 	for _, tt := range tests {
